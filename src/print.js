e36e782ff784b5128511727a607550c8f4c04d74
tanyunxing <tanyunxing@yy.com>
2020-07-21 19:03:46 +0800

    使用 webpack-dev-server

--- src/print.js
+++ src/print.js
@@ -1,6 +1,6 @@
 export default function printMe() {
-  // console.log('I get called from print.js!');
+  console.log('I get called from print.js!');
   
   // 使用 source map追踪到错误和警告在源代码中的原始位置
-   cosnole.error('I get called from print.js!');
+  // cosnole.error('I get called from print.js!');
 }
